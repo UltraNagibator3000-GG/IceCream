b03e01ef7bfcdb7dd12071541e70784e9e774818
UltraNagibator3000-GG <demid.ruban@gmail.com>
2025-03-02 12:27:47 +0300

    add imports

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
+from sqlalchemy import create_engine,Column,String,Integer,REAL,CheckConstraint
+from sqlalchemy.orm import declarative_base,sessionmaker
+from sqlalchemy.ext.hybrid import hybrid_property
 
+database_file = "database.db"
+engine = create_engine(f"sqlite:///{database_file}")
